2ccd8ca550193594809b621bb2904f914cadbd65
Raghu Betina <rbetina@gmail.com>
2013-03-02 15:02:11 -0600

    rails new shoptakara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoptakara::Application.config.secret_token = '24d46a554acdb50e72933397e1a7abbb684469930a7af8d6316b1cbf2ef16adfe6e7f4613293f91e3c0fb50d8c6cd773832529c4ac12ca54cb5ec71369b2e11e'
